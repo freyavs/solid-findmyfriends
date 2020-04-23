a02afae9a8da438384239825186cab2323bd1c0b
Thor Dossche <thor.dossche@ugent.be>
2020-04-24 00:12:14 +0200

    reslove name in vuex

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -1,44 +1,17 @@
 <template>
   <div>
-		<button v-on:click="getName">get</button>
-    <h3 v-if="!loggedIn">{{ namer }}</h3>
-    <h3 v-else>{{ name }}</h3>
-    <h3>{{ webId }}</h3>
+    <h3 v-if="loggedIn">{{ name }}</h3>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
-const $rdf = require('rdflib')
 
 export default {
   computed: mapState({
-    namer: "name",
+    name: "name",
     loggedIn: "loggedIn",
-    webId: "webId",
-  }),
-  data() {
-    return {
-      name: "",
-    };
-  },
-  methods: {
-    getName: function() {
-			console.log('get')
-      // Set up a local data store and associated data fetcher
-      const store = $rdf.graph();
-      const fetcher = new $rdf.Fetcher(store);
-
-      // Load the person's data into the store
-      const person = this.webId;
-      fetcher.load(person).then(() => {
-				const FOAF = $rdf.Namespace("http://xmlns.com/foaf/0.1/");
-        // Display their details
-        const fullName = store.any($rdf.sym(person), FOAF("name"));
-				this.name = fullName.value
-      });
-    },
-  },
+  })
 };
 </script>
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,33 +1,51 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const $rdf = require("rdflib");
+const FOAF = $rdf.Namespace("http://xmlns.com/foaf/0.1/");
+
+const store = $rdf.graph();
+const fetcher = new $rdf.Fetcher(store);
 
 export default new Vuex.Store({
   state: {
-		loggedIn: false,
-		popupUri : 'https://solid.github.io/solid-auth-client/dist/popup.html',
-		webId: '',
-		name: "thor"
+    loggedIn: false,
+    popupUri: "https://solid.github.io/solid-auth-client/dist/popup.html",
+    webId: "",
+    name: "",
+    store: store,
+    fetcher: fetcher,
   },
   mutations: {
-		LOGIN(state, webId){
-			state.loggedIn = true
-			state.webId = webId
-		},
-		LOGOUT(state){
-			state.loggedIn = false
-			state.webId = ''
-		}
+    LOGIN(state, webId) {
+      state.loggedIn = true;
+      state.webId = webId;
+    },
+    LOGOUT(state) {
+      state.loggedIn = false;
+      state.webId = "";
+    },
+    SET_NAME(state, name) {
+      state.name = name;
+    },
   },
   actions: {
-		login({ commit }, webId){
-			commit('LOGIN', webId)
-		},
-		logout({ commit }){
-			commit('LOGOUT')
+    login({ commit, dispatch }, webId) {
+      commit("LOGIN", webId)
+			dispatch('resolveName')
+    },
+    logout({ commit }) {
+      commit("LOGOUT");
+    },
+		resolveName({ commit, state }){
+      const person = state.webId;
+      state.store.fetcher.load(person).then(() => {
+        const fullName = state.store.any($rdf.sym(person), FOAF("name"));
+        commit("SET_NAME", fullName.value);
+      });
 		}
   },
-  modules: {
-  }
-})
+  modules: {},
+});
