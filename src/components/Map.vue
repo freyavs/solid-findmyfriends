fdc62eb6a5653fb2c31e7b0ae525645e9b2178fd
Thor Dossche <thor.dossche@ugent.be>
2020-04-25 17:49:49 +0200

    map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,9 +1,37 @@
 <template>
 	<div>
-		<h1>I'm a map</h1>	
+		<div id="mapContainer"></div>
 	</div>
 </template>
 
-<script></script>
+<script>
+import "leaflet/dist/leaflet.css"
+import L from "leaflet"
 
-<style></style>
+export default {
+data() {
+    return {
+      map: null
+    };
+  },
+  mounted() {
+    this.map = L.map("mapContainer").setView([51.05, 3.71667], 12);
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  },
+  beforeDestroy() {
+    if (this.map) {
+      this.map.remove();
+    }
+  }
+}
+</script>
+
+<style>
+#mapContainer {
+	width: 100%;
+	height: 100%;
+}
+</style>
