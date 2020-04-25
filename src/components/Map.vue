398843da0c435a69c6d8f36158347d8991569479
Thor Dossche <thor.dossche@ugent.be>
2020-04-25 19:02:11 +0200

    marker

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,24 +1,51 @@
 <template>
-	<div>
-		<div id="mapContainer"></div>
-	</div>
+  <div>
+    <div id="mapContainer"></div>
+  </div>
 </template>
 
 <script>
-import "leaflet/dist/leaflet.css"
-import L from "leaflet"
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { mapState } from "vuex";
 
 export default {
-data() {
+  computed: mapState(["currentLocation"]),
+  data() {
     return {
-      map: null
+      map: null,
+      marker: null
+      //this.marker = L.marker(this.currentLocation.latlng).addTo(this.map)
     };
+  },
+  watch: {
+    currentLocation: function() {
+      if (this.currentLocation != null) {
+        let smallIcon = new L.Icon({
+          iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png",
+          iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon-2x.png",
+          iconSize: [25, 41],
+          iconAnchor: [12, 41],
+          popupAnchor: [1, -34],
+          shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+          shadowSize: [41, 41]
+        });
+
+        this.marker = L.marker([
+            this.currentLocation.coords.latitude,
+            this.currentLocation.coords.longitude
+          ], { icon: smallIcon }).addTo(this.map)
+
+      }else{
+				this.map.removeLayer(this.marker)
+			}
+    }
   },
   mounted() {
     this.map = L.map("mapContainer").setView([51.05, 3.71667], 12);
-    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
   },
   beforeDestroy() {
@@ -26,12 +53,12 @@ data() {
       this.map.remove();
     }
   }
-}
+};
 </script>
 
 <style>
 #mapContainer {
-	width: 100%;
-	height: 100%;
+  width: 100%;
+  height: 100%;
 }
 </style>
