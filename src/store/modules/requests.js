53f67ab016633cdf64bc581e8e7aebc1d7b991b2
fvspeybr <freya.vanspeybroeck@ugent.be>
2020-05-12 20:48:04 +0200

    fixes

--- src/store/modules/requests.js
+++ src/store/modules/requests.js
@@ -1,13 +1,13 @@
 
-var rdf = require('rdflib')
-var sn = require('@/lib/solid-notifs.js')
+const rdf = require('rdflib')
+const sn = require('@/lib/solid-notifs.js')
 
 const auth = require('solid-auth-client')
 const FC	 = require('solid-file-client')
 const fc	 = new FC( auth )
 
-var webClient = require('solid-web-client')(rdf)
-var options = {
+const webClient = require('solid-web-client')(rdf)
+const options = {
   "webClient" : webClient
 }
 export const state = {
@@ -18,23 +18,23 @@ export const mutations = {
 }
 
 export const actions = {
-    async fetchRequests({rootState}){
+    fetchRequests({rootState}){
         sn.list(rootState.webId, options).then(contains => {
             for (let message of contains){
                 fc.readFile(message).then( m => console.log(m))
             }
         })
     },
-    requestLocation( { state } , friendWebId){
+    requestLocation( { rootState} ,friendWebId){
         console.log("requesting location of " + friendWebId )
+        console.log("my id: " + rootState.webId)
 
-          let payload = {
+          let payload = `{
             "@context": "https://www.w3.org/ns/activitystreams",
             "@id": "",
             "type": "FindMyFriendsRequest",
-            "from": state.webId
-          }
-          sn.send(friendWebId, payload, options)   
+            "from": ${rootState.webId} }`
+          sn.send(friendWebId.toString(), payload, options) 
     }
 
 }
