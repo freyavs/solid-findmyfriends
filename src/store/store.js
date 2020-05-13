133832501fe1d4df4f6f6e2336db93eeec8bdcdf
fvspeybr <freya.vanspeybroeck@ugent.be>
2020-05-13 13:55:19 +0200

    weg

--- src/store/store.js
+++ src/store/store.js
@@ -7,13 +7,6 @@ import * as requests from '@/store/modules/requests.js'
 
 Vue.use(Vuex);
 
-const auth = require('solid-auth-client')
-const FC	 = require('solid-file-client')
-const fc	 = new FC( auth )
-
-const {default: data} = require('@solid/query-ldflex')
-
-const N3 = require('n3');
 
 export default new Vuex.Store({
 	state: {
@@ -47,44 +40,7 @@ export default new Vuex.Store({
 			const locationFile = await tools.setLocationFile(state.webId)
 			commit("SET_LOCATION_FILE", locationFile)
 			dispatch("fetchFriendsPermissions")
-		},
-		async createLocationFile({state, commit}){
-			//verkrijg publicTypeIndex			
-			const person = data[state.webId]
-			const registry = await person['http://www.w3.org/ns/solid/terms#publicTypeIndex']
-			//verkrijg podroot
-			const podRoot = state.webId.replace(/\/profile.*/, '/')
-			
-			//maak locationfile aan
-			fc.createFile(podRoot + "public/location.ttl", "", "text/turtle")
-				.then(() => {
-					//update de publicTypeIndex met de nieuwe file
-					const query = `
-					INSERT DATA{
-						@prefix solid: <http://www.w3.org/ns/solid/terms#>.
-						@prefix wgs: <http://www.w3.org/2003/01/geo/wgs84_pos#>.
-						<findmyfriendslocation> a solid:TypeRegistration;
-							solid:forClass wgs:Point;
-							solid:instance </public/location.ttl>.	
-					}
-					`
-					auth.fetch(registry, {
-						method: 'PATCH',
-						headers: { 'Content-Type': 'application/sparql-update' },
-						body: query,
-						credentials: 'include',
-					})
-						.then(() => commit("SET_LOCATION_FILE", podRoot + "public/location.ttl"))
-						.catch(error => {
-						console.log("Failed to update publicTypeIndex")
-						console.log(error)	
-						})
-				})
-				.catch(error => {
-					console.log("Failed to create location file")
-					console.log(error)
-				})
-    }
+		}
 	},
 	modules: {
 		location,
